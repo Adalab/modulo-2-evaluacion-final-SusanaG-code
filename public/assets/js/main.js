851bf6ff08718efb530f5318bec276ce532c7a5a
Susana <su.gonzalez.vejo@gmail.com>
2020-10-26 13:04:19 +0100

    Add null

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,58 +3,39 @@
 const search = document.querySelector('.js-btn');
 const inputElement = document.querySelector('.js-input');
 const showList = document.querySelector('.js-list');
-
-
+let userFavUl = document.querySelector('.js-fav-user');
 
 
 let show = [];
 
-
 function getShowList(event) {
-    event.preventDefault();
+    // event.preventDefault();
     const userValue = inputElement.value;
     fetch(`http://api.tvmaze.com/search/shows?q=${userValue}`)
         .then(response => response.json())
         .then(data => {
             show = data;
-            console.log(show);
+
+            paintShowCard();
         })
         .catch(error => {
-            console.error('Se ha producido un error:', error);;
+            console.error('Se ha producido un error:', error);
         })
-    paintShowCard();
 
 }
-
 
 search.addEventListener('click', getShowList);
 
 function paintShowCard() {
-
+    showList.innerHTML = '';
     for (let i = 0; i < show.length; i++) {
-        let classFav;
-        const favoriteIndex = fav.indexOf(i);
-        const favorite = favoriteIndex !== -1;
-        if (favorite === false) {
-            classFav = "favColor";
-            ˆ
-        }
-        else {
-            classF = "";
-        }
 
-        const arrayShowImg = show[i].show.image.medium;
+        const arrayShowImg = show[i].show.image;
         const arrayShowName = show[i].show.name;
         const arrayShowId = show[i].show.id;
 
         const newImg = createElements(arrayShowName, arrayShowId, arrayShowImg);
 
-        if (arrayShowImg === null) {
-            newImg = 'https://via.placeholder.com/210x295/ffffff/666666/?';
-            console.log('imagen');
-        } else {
-            newImg;
-        }
         listenToFavorite();
 
     }
@@ -67,9 +48,15 @@ function paintShowCard() {
         showList.appendChild(newList);
         newList.setAttribute('class', 'showCard js-shows classFav');
         newList.setAttribute('id', arrayShowId);
-
         const newImg = document.createElement('img');
-        newImg.setAttribute('src', arrayShowImg);
+        let urlImg = '';
+        if (arrayShowImg === null) {
+            urlImg = 'https://via.placeholder.com/210x295/ffffff/666666/?';
+
+        } else {
+            urlImg = arrayShowImg.medium;
+        }
+        newImg.setAttribute('src', urlImg);
         newImg.setAttribute('alt', 'Tv show');
         newList.appendChild(newImg);
         return newImg;
@@ -80,31 +67,42 @@ function paintShowCard() {
 
 
 
-'use strict';
 
+
+
+
+
+
+
+'use strict';
 
 //query selectors
 let fav = [];
 
+
 //añadir favoritos en un array
 function favoriteShows(ev) {
-    let classFav;
-    const clicked = ev.currentTarget.id;
-    console.log(clicked);
+    const clicked = parseInt(ev.currentTarget.id);
+    const clickedElement = ev.currentTarget;
 
-    const indexFav = fav.indexOf(clicked);
+    //const indexFav = fav.indexOf(clicked);
+    const indexFav = fav.findIndex(favItem => favItem.show.id === clicked)
     const clickFavorite = indexFav !== -1;
     if (clickFavorite === false) {
-        fav.push(clicked);
+
+        const foundShow = show.find(showItem => showItem.show.id === clicked)
+        fav.push(foundShow);
+        clickedElement.classList.add('showCardRev');
 
     }
     else {
         fav.splice(indexFav, 1);
+        clickedElement.classList.remove('showCardRev');
     }
+    paintFav();
+    setLocalStorage();
 
 }
-
-
 
 function listenToFavorite() {
     const favItems = document.querySelectorAll('.js-shows');
@@ -115,7 +113,60 @@ function listenToFavorite() {
 
 
 
- // setInLocalStorage();
+
+function paintFav() {
+    userFavUl.innerHTML = ''
+    for (let i = 0; i < fav.length; i++) {
+
+        const arrayFavImg = fav[i].show.image;
+        const arrayFavName = fav[i].show.name;
+        const arrayFavId = fav[i].show.id;
+
+        const userFavLi = document.createElement('li');
+        const contentFavList = document.createTextNode(arrayFavName);
+        userFavLi.appendChild(contentFavList);
+        userFavUl.appendChild(userFavLi);
+        userFavLi.setAttribute('id', arrayFavId);
+        userFavLi.setAttribute('class', 'favLi');
+        const userFavImg = document.createElement('img');
+        let urlFav = '';
+        if (arrayFavImg === null) {
+            console.log(arrayFavImg);
+            urlFav = 'https://via.placeholder.com/210x295/ffffff/666666/?';
+
+
+        } else {
+            urlFav = arrayFavImg.medium;
+        }
+        userFavImg.setAttribute('src', urlFav);
+        userFavLi.appendChild(userFavImg);
+    }
+}
+
+
+
+
+
+
+'use strict';
+
+function setLocalStorage() {
+    localStorage.setItem('fav', JSON.stringify(fav));
+}
+function getLocalStorage() {
+    const favLocalStorage = JSON.parse(localStorage.getItem(fav));
+    const favLocalStorageJ = JSON.parse(favLocalStorage)
+    if (favLocalStorageJ === null) {
+        getShowList();
+    } else {
+        fav = favLocalStorageJ;
+    }
+}
+getLocalStorage();
+
+
+'use strict';
+
 
 
 
