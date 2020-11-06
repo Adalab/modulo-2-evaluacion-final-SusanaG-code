27afc0ebeb9373c99a000a4b12733720bfc2cc46
Susana <su.gonzalez.vejo@gmail.com>
2020-11-06 19:19:39 +0100

    Fix

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,96 @@
+"use strict";
+
+const search = document.querySelector(".js-btn");
+const inputElement = document.querySelector(".js-input");
+const showList = document.querySelector(".js-list");
+console.log(showList);
+let userFavUl = document.querySelector(".js-fav-user");
+
+let show = [];
+
+function getShowList(event) {
+  // event.preventDefault();
+  const userValue = inputElement.value;
+  fetch(`http://api.tvmaze.com/search/shows?q=${userValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      show = data;
+      paintShowCard();
+      paintConsole();
+      paintFav();
+    })
+    .catch((error) => {
+      console.error("Se ha producido un error:", error);
+    });
+}
+
+search.addEventListener("click", getShowList);
+
+function paintShowCard() {
+  showList.innerHTML = "";
+  for (let i = 0; i < show.length; i++) {
+    const arrayShowImg = show[i].show.image;
+    const arrayShowName = show[i].show.name;
+    const arrayShowId = show[i].show.id;
+    const arrayShowGenre = show[i].show.genres;
+
+    const newImg = createElements(
+      arrayShowName,
+      arrayShowId,
+      arrayShowImg,
+      arrayShowGenre
+    );
+
+    listenToFavorite();
+  }
+
+  function createElements(
+    arrayShowName,
+    arrayShowId,
+    arrayShowImg,
+    arrayShowGenre
+  ) {
+    const newList = document.createElement("li");
+    const newContentList = document.createTextNode(arrayShowName);
+    const newContentListGenre = document.createTextNode(
+      arrayShowGenre.join(" - ")
+    );
+    newList.appendChild(newContentListGenre);
+    newList.appendChild(newContentList);
+    showList.appendChild(newList);
+    newList.setAttribute("class", "showCard js-shows classFav");
+    newList.setAttribute("id", arrayShowId);
+    const newImg = document.createElement("img");
+    let urlImg = "";
+    if (arrayShowImg === null) {
+      urlImg = "https://via.placeholder.com/210x295/ffffff/666666/?";
+    } else {
+      urlImg = arrayShowImg.medium;
+    }
+    newImg.setAttribute("src", urlImg);
+    newImg.setAttribute("alt", "Tv show");
+    newList.appendChild(newImg);
+    return newImg;
+  }
+}
+
+"use strict";
+
+function setLocalStorage() {
+  localStorage.setItem("fav", JSON.stringify(fav));
+}
+function getLocalStorage() {
+  const favLocalStorage = localStorage.getItem("fav");
+  const favLocalStorageJ = JSON.parse(favLocalStorage);
+  if (favLocalStorageJ === null) {
+    getShowList();
+  } else {
+    fav = favLocalStorageJ;
+  }
+}
+
+getLocalStorage();
+
 'use strict';
 
 const search = document.querySelector('.js-btn');
@@ -163,21 +256,6 @@ function getLocalStorage() {
 }
 
 getLocalStorage();
-
-'use strict';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"use strict";
 
 //# sourceMappingURL=main.js.map
---
+++ src/js/3ls.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function setLocalStorage() {
+  localStorage.setItem("fav", JSON.stringify(fav));
+}
+function getLocalStorage() {
+  const favLocalStorage = localStorage.getItem("fav");
+  const favLocalStorageJ = JSON.parse(favLocalStorage);
+  if (favLocalStorageJ === null) {
+    getShowList();
+  } else {
+    fav = favLocalStorageJ;
+  }
+}
+
+getLocalStorage();
